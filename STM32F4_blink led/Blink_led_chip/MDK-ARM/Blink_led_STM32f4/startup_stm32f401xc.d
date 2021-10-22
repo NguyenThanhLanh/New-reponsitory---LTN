f9ef0f0354a5b491d2c210038a8905056960f4e3
Lanhdz <thanhlanh12a3@gmail.com>
2021-10-23 00:01:01 +0700

    Bai test STM32F4_401CB

---
+++ STM32F4_blink led/Blink_led_chip/MDK-ARM/Blink_led_STM32f4/startup_stm32f401xc.d
@@ -0,0 +1,1 @@
+blink_led_stm32f4\startup_stm32f401xc.o: startup_stm32f401xc.s
---
+++ STM32F4_blink led/Blink_led_chip/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blink_led_STM32f4' 
+ * Target:  'Blink_led_STM32f4' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
